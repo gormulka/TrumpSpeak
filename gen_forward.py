e35d13d6cf462d471f6a42ea230699ba99a804da
gormulka <trentr@hawaii.edu>
2020-07-13 13:23:37 -0400

    derp

--- gen_forward.py
+++ gen_forward.py
@@ -153,7 +153,7 @@ if __name__ == '__main__':
             v_type = 'wavernn_unbatched'
 
         if input_text:
-            save_path = paths.forward_output/f'{input_text[:10]}_{args.alpha}_{v_type}_{tts_k}k.wav'
+            save_path = '/content/output/ + f'{input_text[:40]}.wav'
         else:
             save_path = paths.forward_output/f'{i}_{v_type}_{tts_k}ko.wav'
 
